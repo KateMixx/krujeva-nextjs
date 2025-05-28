466450bfcc6ab707a9129d934e7d34aa1d75377c
KateMixx <65362838+KateMixx@users.noreply.github.com>
2025-05-29 00:09:12 +0300

    Fix for img

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,25 +1,25 @@
 export enum Images {
-    Header = '/images/header.jpg',
-    Icon = '/images/icons/favicon.png'
+    Header = './images/header.jpg',
+    Icon = './images/icons/favicon.png'
 }
 
 export enum TypesPortfolio{
-    Bedroom = '/images/types-portfolio/bedroom.jpg',
-    Cabinets = '/images/types-portfolio/cabinets.jpg',
-    ChildrenRoom = '/images/types-portfolio/children-room.jpg',
-    WoodHouse = '/images/types-portfolio/wood-house.jpg',
-    Kitchen = '/images/types-portfolio/kitchen.jpg',
-    LivingRoom = '/images/types-portfolio/living-room.jpg',
-    WeddingDecoration = '/images/types-portfolio/wedding-decoration.jpg',
-    CorporateOrders = '/images/types-portfolio/corporate-orders.jpg',
-    InteriorDecor = '/images/types-portfolio/interior-decor.jpg',
+    Bedroom = './images/types-portfolio/bedroom.jpg',
+    Cabinets = './images/types-portfolio/cabinets.jpg',
+    ChildrenRoom = './images/types-portfolio/children-room.jpg',
+    WoodHouse = './images/types-portfolio/wood-house.jpg',
+    Kitchen = './images/types-portfolio/kitchen.jpg',
+    LivingRoom = './images/types-portfolio/living-room.jpg',
+    WeddingDecoration = './images/types-portfolio/wedding-decoration.jpg',
+    CorporateOrders = './images/types-portfolio/corporate-orders.jpg',
+    InteriorDecor = './images/types-portfolio/interior-decor.jpg',
 }
 
 export enum ClientsLogo {
-    Chapaev = '/images/logos/chapaev.jpg',
-    Filarmonia = '/images/logos/filarmonia.jpg',
-    Pozhtehnika = '/images/logos/pozhtehnika.jpg',
-    RZD = '/images/logos/rzd.jpg',
-    UsadbaTerema = '/images/logos/usadba_terema.jpg',
-    VolzhskijPekar = '/images/logos/Volzhskij-pekar.jpg',
+    Chapaev = './images/logos/chapaev.jpg',
+    Filarmonia = './images/logos/filarmonia.jpg',
+    Pozhtehnika = './images/logos/pozhtehnika.jpg',
+    RZD = './images/logos/rzd.jpg',
+    UsadbaTerema = './images/logos/usadba_terema.jpg',
+    VolzhskijPekar = './images/logos/Volzhskij-pekar.jpg',
 }
